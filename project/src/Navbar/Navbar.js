20ee58af076f849f362e130357b7a6b5d18ff641
shkelzenjashari <jasharishkelzen@gmail.com>
2023-12-14 01:19:34 +0100

    hamburger-menu-updated

--- project/src/Navbar/Navbar.js
+++ project/src/Navbar/Navbar.js
@@ -5,33 +5,52 @@ import Contact from "../Pages/Contact";
 import About from "../Pages/About";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-logo">
         <img src={logo} alt="logo" />
       </div>
 
-      <div className="navbar-list">
+      <div className={`navbar-list ${menuVisible ? "visible" : ""}`}>
         <ul>
           <li>
-            <Link to={Home}>Home</Link>
+            <Link to={Home} onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" onClick={toggleMenu}>
+              About
+            </Link>
           </li>
         </ul>
       </div>
 
-      <div className="user-login">
+      <div className={`user-login ${menuVisible ? "visible" : ""}`}>
         <img src={userLogin} alt="user-login" />
-        <button>
-          <Link to="/login">Sign in</Link>
-        </button>
+        <button>Login</button>
+      </div>
+      <div
+        className={`menu-icon ${menuVisible ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
       </div>
     </div>
   );
