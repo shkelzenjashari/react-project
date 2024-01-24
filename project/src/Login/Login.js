79f094ebe7c5e0bb0589cf30a125ee1006a5296f
shkelzenjashari <jasharishkelzen@gmail.com>
2024-01-24 13:33:11 +0100

    positive-bar-me-pimpin-part2

--- project/src/Login/Login.js
+++ project/src/Login/Login.js
@@ -1,79 +1,177 @@
+// import React, { useState } from "react";
+// import Navbar from "../Navbar/Navbar";
+// import "./Login.css";
+// import image from "../Images/typing.jpg";
+// import { Link, useNavigate } from "react-router-dom";
+// import useFormValidation from "../Hooks/useFormValidation";
+
+// // const Login = () => {
+// //   const [isLoggedIn, setIsLoggedIn] = useState(false);
+// //   const navigate = useNavigate();
+
+// //   const { errors, validateForm } = useFormValidation();
+
+// //   const [formData, setFormData] = useState({
+// //     email: "",
+// //     password: "",
+// //   });
+
+// //   const handleSignUpClick = () => {
+// //     navigate("/signup");
+// //   };
+
+// //   const handleSubmit = (e) => {
+// //     e.preventDefault();
+
+// //     if (validateForm(formData, "signin")) {
+// //       console.log("Form submitted successfully");
+// //       setIsLoggedIn(true);
+// //     }
+// //   };
+
+// //   const handleChange = (evt) => {
+// //     setFormData({ ...formData, [evt.target.name]: evt.target.value });
+// //   };
+
+// //   return (
+// //     <>
+// //       <Navbar />
+// //       <div className="login-container">
+// //         <div className="form-container">
+// //           <form onSubmit={handleSubmit}>
+// //             <h2>Sign In</h2>
+// //             <label>Email:</label>
+// //             <input
+// //               type="email"
+// //               name="email"
+// //               value={formData.email}
+// //               onChange={handleChange}
+// //               required
+// //             />
+// //             {errors.email && <p className="error">{errors.email}</p>}
+// //             <label>Password:</label>
+// //             <input
+// //               type="password"
+// //               name="password"
+// //               value={formData.password}
+// //               onChange={handleChange}
+// //               required
+// //             />
+// //             {errors.password && <p className="error">{errors.password}</p>}
+// //             <button className="loginFormButton" type="submit">
+// //               Sign in
+// //             </button>
+// //           </form>
+// //         </div>
+// //         <div className="image-container">
+// //           <img src={image} alt="Login" />
+// //           <button
+// //             className="loginFormButton switch-button login"
+// //             onClick={handleSignUpClick}
+// //           >
+// //             Don't have an account? Sign Up
+// //           </button>
+// //         </div>
+// //       </div>
+// //     </>
+// //   );
+// // };
+
+// export default Login;
 import React, { useState } from "react";
-import Navbar from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import image from "../Images/typing.jpg";
-import { Link, useNavigate } from "react-router-dom";
-import useFormValidation from "../Hooks/useFormValidation";
 
 const Login = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
 
-  const { errors, validateForm } = useFormValidation();
+  const submit = async (event) => {
+    event.preventDefault();
+    const fetchUrl = "http://localhost:8000/api/user/login";
 
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+    try {
+      const response = await fetch(fetchUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
 
-  const handleSignUpClick = () => {
-    navigate("/signup");
-  };
+      if (!response.ok) {
+        const responseData = await response.json();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+        if (response.status === 401) {
+          setEmailError(responseData.emailError || null);
+          setPasswordError(
+            responseData.passwordError ||
+              "Incorrect password. Please try again."
+          );
+        } else {
+          throw new Error("Login failed");
+        }
+      } else {
+        setEmailError(null);
+        setPasswordError(null);
 
-    if (validateForm(formData, "signin")) {
-      console.log("Form submitted successfully");
-      setIsLoggedIn(true);
+        const data = await response.json();
+        localStorage.setItem("user", JSON.stringify(data));
+
+        navigate("/home");
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleChange = (evt) => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  const handleChange = (type) => (e) => {
+    setCredentials({ ...credentials, [type.toLowerCase()]: e.target.value });
   };
 
   return (
-    <>
-      <Navbar />
-      <div className="login-container">
-        <div className="form-container">
-          <form onSubmit={handleSubmit}>
-            <h2>Sign In</h2>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            {errors.email && <p className="error">{errors.email}</p>}
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            {errors.password && <p className="error">{errors.password}</p>}
-            <button className="loginFormButton" type="submit">
-              Sign in
-            </button>
+    <div className="container">
+      <div className="signinBx">
+        <div className="imgBx">
+          <img src={""} alt="Background" />
+        </div>
+        <div className="formBx">
+          <form onSubmit={submit} className="signinForm">
+            <h3>Sign In</h3>
+
+            <div className="formGroup mb-3">
+              <label>Email</label>
+              <input
+                type="text"
+                className={`form-control ${emailError ? "is-invalid" : ""}`}
+                placeholder="Enter Email"
+                onChange={handleChange("Email")}
+              />
+              {emailError && <div className="error-message">{emailError}</div>}
+            </div>
+            <div className="formGroup mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className={`form-control ${passwordError ? "is-invalid" : ""}`}
+                placeholder="Enter password"
+                onChange={handleChange("Password")}
+              />
+              {passwordError && (
+                <div className="error-message">{passwordError}</div>
+              )}
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
           </form>
         </div>
-        <div className="image-container">
-          <img src={image} alt="Login" />
-          <button
-            className="loginFormButton switch-button login"
-            onClick={handleSignUpClick}
-          >
-            Don't have an account? Sign Up
-          </button>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- project/src/SignUp/SignUp.js
+++ project/src/SignUp/SignUp.js
@@ -1,96 +1,215 @@
+// import React, { useState } from "react";
+// import Navbar from "../Navbar/Navbar";
+// import "./SignUp.css";
+// import image from "../Images/typing.jpg";
+// import { useNavigate } from "react-router-dom";
+// import useFormValidation from "../Hooks/useFormValidation";
+
+// const SignUp = () => {
+//   const navigate = useNavigate();
+//   const { errors, validateForm } = useFormValidation();
+
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     surname: "",
+//     email: "",
+//     password: "",
+//   });
+
+//   const handleSignUpClick = () => {
+//     navigate("/login");
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (validateForm(formData, "signup")) {
+//       console.log("Form submitted successfully");
+//     }
+//   };
+
+//   const handleChange = (evt) => {
+//     setFormData({ ...formData, [evt.target.name]: evt.target.value });
+//   };
+
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="login-container">
+//         <div className="image-container">
+//           <img src={image} alt="Login" />
+//           <button
+//             className="loginFormButton switch-button signup"
+//             onClick={handleSignUpClick}
+//           >
+//             Already have an account? Log In
+//           </button>
+//         </div>
+//         <div className="form-container">
+//           <form onSubmit={handleSubmit}>
+//             <h2>Sign Up</h2>
+//             <label>Name:</label>
+//             <input
+//               type="text"
+//               name="name"
+//               value={formData.name}
+//               onChange={handleChange}
+//               required
+//             />
+//             {errors.name && <p className="error">{errors.name}</p>}
+//             <label>Surname:</label>
+//             <input
+//               type="text"
+//               name="surname"
+//               value={formData.surname}
+//               onChange={handleChange}
+//               required
+//             />
+//             {errors.surname && <p className="error">{errors.surname}</p>}
+//             <label>Email:</label>
+//             <input
+//               type="email"
+//               name="email"
+//               value={formData.email}
+//               onChange={handleChange}
+//               required
+//             />
+//             {errors.email && <p className="error">{errors.email}</p>}
+//             <label>Password:</label>
+//             <input
+//               type="password"
+//               name="password"
+//               value={formData.password}
+//               onChange={handleChange}
+//               required
+//             />
+//             {errors.password && <p className="error">{errors.password}</p>}
+//             <button className="loginFormButton" type="submit">
+//               Sign Up
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SignUp;
 import React, { useState } from "react";
-import Navbar from "../Navbar/Navbar";
+import { Link, useNavigate } from "react-router-dom";
 import "./SignUp.css";
-import image from "../Images/typing.jpg";
-import { useNavigate } from "react-router-dom";
-import useFormValidation from "../Hooks/useFormValidation";
 
 const SignUp = () => {
-  const navigate = useNavigate();
-  const { errors, validateForm } = useFormValidation();
-
   const [formData, setFormData] = useState({
-    name: "",
-    surname: "",
     email: "",
     password: "",
+    age: "",
+    name: "",
+    surname: "",
   });
 
-  const handleSignUpClick = () => {
-    navigate("/login");
-  };
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const submit = async (event) => {
+    event.preventDefault();
+    const fetchUrl = "http://localhost:8000/api/user/";
+    try {
+      const response = await fetch(fetchUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    if (validateForm(formData, "signup")) {
-      console.log("Form submitted successfully");
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      const data = await response.json();
+      localStorage.setItem("user", JSON.stringify(data));
+
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+      setFormData({
+        ...formData,
+        error: "Registration failed. Please try again.",
+      });
     }
   };
 
-  const handleChange = (evt) => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  const handleChange = (type) => (e) => {
+    setFormData({ ...formData, [type]: e.target.value });
   };
 
   return (
-    <>
-      <Navbar />
-      <div className="login-container">
-        <div className="image-container">
-          <img src={image} alt="Login" />
-          <button
-            className="loginFormButton switch-button signup"
-            onClick={handleSignUpClick}
-          >
-            Already have an account? Log In
-          </button>
-        </div>
-        <div className="form-container">
-          <form onSubmit={handleSubmit}>
-            <h2>Sign Up</h2>
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-            {errors.name && <p className="error">{errors.name}</p>}
-            <label>Surname:</label>
-            <input
-              type="text"
-              name="surname"
-              value={formData.surname}
-              onChange={handleChange}
-              required
-            />
-            {errors.surname && <p className="error">{errors.surname}</p>}
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-            {errors.email && <p className="error">{errors.email}</p>}
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            {errors.password && <p className="error">{errors.password}</p>}
-            <button className="loginFormButton" type="submit">
-              Sign Up
-            </button>
-          </form>
+    <form onSubmit={submit}>
+      <div className="container">
+        <div className="signupBx">
+          <div className="imgBx">
+            <img src={"img2"} alt="Background" />
+          </div>
+          <div className="formBx">
+            <h3>Sign Up</h3>
+            <div className="mb-3">
+              <label>Email</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Email"
+                onChange={handleChange("email")}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={handleChange("password")}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Name"
+                onChange={handleChange("name")}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Surname</label>
+              <input
+                type="tel"
+                className="form-control"
+                placeholder="Enter Surname"
+                onChange={handleChange("surname")}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Age</label>
+              <input
+                type="tel"
+                className="form-control"
+                placeholder="Enter Age"
+                onChange={handleChange("age")}
+              />
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+              <p>
+                Already have an account?
+                <Link to="/login">Sign In</Link>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
